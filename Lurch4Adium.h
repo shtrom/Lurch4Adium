7050ae258e60437cc8d06c4cea6e1f65ecb0f648
Olivier Mehani <olivier.mehani@learnosity.com>
2017-05-05 12:54:59 +1000

    Add stub files
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

---
+++ Lurch4Adium.h
@@ -0,0 +1,14 @@
+//
+//  Lurch4Adium.h
+//  Lurch4Adium
+//
+//  Created by Olivier Mehani on 5/5/17.
+//  Copyright © 2017 Olivier Mehani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Adium/AIPlugin.h>
+
+@interface Lurch4Adium : NSObject <AIPlugin>
+
+@end
---
+++ Lurch4Adium.m
@@ -0,0 +1,23 @@
+//
+//  Lurch4Adium.m
+//  Lurch4Adium
+//
+//  Created by Olivier Mehani on 5/5/17.
+//  Copyright © 2017 Olivier Mehani. All rights reserved.
+//
+
+#import "Lurch4Adium.h"
+
+@implementation Lurch4Adium
+
+- (void) installPlugin
+{
+    
+}
+
+- (void) uninstallPlugin
+{
+    
+}
+
+@end
